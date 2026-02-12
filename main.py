efbcd0d94df9e0ba2ba5ff2c50df73c7317d90c5
akash <akashnagar97846@gmail.com>
2026-02-12 15:48:04 +0530

    data

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from ml!")
+import pandas as pd
+import numpy as np
 
+df=pd.read_csv('covid_toy.csv')
+print(df.head(2))
 
-if __name__ == "__main__":
-    main()
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+df['gender']=le.fit_transform(df['gender'])
+df['cough']=le.fit_transform(df['cough'])
+df['city']=le.fit_transform(df['city'])
+
+print(df.head(2))
